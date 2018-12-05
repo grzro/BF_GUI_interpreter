1af0396d160f3f4f6c0b6bf4de864d912d2fc418
g.romanczyk <grzesiek.romanczyk@gmail.com>
2018-12-05 19:39:36 +0100

    Create Flag.java
    Flags to exchange information between GUI and parser

---
+++ src/Flag.java
@@ -0,0 +1,17 @@
+public class Flag {
+	static final int GET_CHAR = 1;
+	static final int FINISH = 2;
+	static final int IN_PROGRESS = 3;
+	static final int LOOP_ERROR = 4;
+	static final int OUT_OF_MEM_BOUNDS = 5;
+	static final int DOING_NOTHING = 6;
+	static final int PARSED = 7;
+	static final int TO_PRINT = 8;
+	static final int NO_CODE_TO_PARSE = 9;
+	
+	int current = DOING_NOTHING;
+	
+	public Flag(int state) {
+		current = state;
+	}
+}
